609911e39fe3630e9964dc3d40396339cb2563fa
Chandrima Ghatak <chandrima.cg@yahoo.com>
2026-07-06 22:30:53 -0600

    setup helper functions

---
+++ ELD/Gateway/setup.h
@@ -0,0 +1,14 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+/***********************************************************************
+ * setup.h
+ *
+ * System initialization for the CAN Logger ELD Prototype.
+ *
+ *
+ ***********************************************************************/
+
+void setupELD(void);
+
+#endif
